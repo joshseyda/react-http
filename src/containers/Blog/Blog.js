5a0fd29733e008c1560c67af793042a62af81f56
joshseyda <josh.seyda@gmail.com>
2019-02-21 15:52:40 -0500

    Click on a post title to view full post prelim

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from 'axios';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
     componentDidMount (){
         axios.get('http://jsonplaceholder.typicode.com/posts')
@@ -18,16 +19,25 @@ class Blog extends Component {
                 const updatedPosts = posts.map(post => {
                     return {
                         ...post,
-                        author: "Max"
+                        author: 'Max'
                     }
                 })
                 this.setState({posts: updatedPosts});
                 // console.log(response);
             });
     }
+
+    postSelectedHandler= (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>;
+            return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={()=> this.postSelectedHandler(post.id)}/>;
         });
         return (
             <div>
@@ -35,7 +45,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
