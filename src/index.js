23098a44573287c03ddb4d450387cb9eb30103fc
joshseyda <josh.seyda@gmail.com>
2019-02-21 16:41:25 -0500

    Add axios Interceptors to validate requests and handle errors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+  console.log(request);
+  //Here you can Edit the Request config, you MUST return the request or your application will not have data!
+  return request;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  console.log(response);
+  return response;
+}, error => {
+  console.log(error);
+  return Promise.reject(error);
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
