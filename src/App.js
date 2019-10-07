d87f9d30cd1ae0c171f9435e372a6c1006b69d7d
joshseyda <josh.seyda@gmail.com>
2019-10-07 13:58:07 -0400

    small note about serving from sub directory in app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter basename='/my-app'/> used if you are serving your app from a sub directory!
       <BrowserRouter>
         <div className="App">
           <Blog />
